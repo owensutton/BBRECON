da6c2b4a1c263710851859a4f35f409348a1f1ed
owensutton <45274107+owensutton@users.noreply.github.com>
2023-08-07 17:06:01 -0400

    Initial Creation

---
+++ src/Main.py
@@ -0,0 +1,16 @@
+#
+#   Name: BBRECON
+#   Created By: Owen Sutton
+#   Description: Bug Bounty Recon tool. 
+#
+#   Version = 1.00
+#
+
+
+def main():
+    print("Start of BBRECON....\n")
+    print("Enter Target location: ")
+
+
+if __name__ == "__main__":
+    main()
